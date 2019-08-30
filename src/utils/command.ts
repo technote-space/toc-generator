8766864d9f5844d6cc5a6501262e2cd2bade977c
Technote <technote.space@gmail.com>
2019-08-31 00:19:26 +0900

    fix: command

--- src/utils/command.ts
+++ src/utils/command.ts
@@ -39,7 +39,7 @@ const getCurrentBranchName = async (workDir: string): Promise<string> => {
 
 const runDocToc = async (workDir: string): Promise<boolean> => {
     const args = getDocTocArgs();
-    await execAsync('cd ${workDir} && yarn add doctoc');
+    await execAsync(`yarn --cwd ${workDir} add doctoc`);
     await execAsync(`cd ${workDir} && ./node_modules/.bin/doctoc ${args}`);
     return true;
 };
