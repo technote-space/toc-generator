c6a1632c5009343f961fd35427c73cc6e6b9df97
Technote <technote.space@gmail.com>
2019-10-05 18:35:45 +0900

    feat: set pr ref to pull head

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import { Logger, Utils, ApiHelper } from '@technote-space/github-action-helper';
 import { getChangedFiles } from './utils/command';
 import { isTargetContext, getCommitMessage, getWorkDir } from './utils/misc';
 
-const {showActionInfo} = Utils;
+const {showActionInfo, getPrHeadRef} = Utils;
 
 /**
  * run
@@ -13,18 +13,19 @@ const {showActionInfo} = Utils;
 async function run(): Promise<void> {
 	try {
 		const logger = new Logger();
-		showActionInfo(path.resolve(__dirname, '..'), logger, context);
+		const _context = Object.assign({}, context, {ref: getPrHeadRef(context)});
+		showActionInfo(path.resolve(__dirname, '..'), logger, _context);
 
-		if (!isTargetContext(context)) {
+		if (!isTargetContext(_context)) {
 			logger.info('This is not target event.');
 			return;
 		}
 
-		const files = await getChangedFiles(context);
+		const files = await getChangedFiles(_context);
 		if (false === files) {
 			return;
 		}
-		await (new ApiHelper(logger)).commit(getWorkDir(), getCommitMessage(), files, new GitHub(getInput('GITHUB_TOKEN', {required: true})), context);
+		await (new ApiHelper(logger)).commit(getWorkDir(), getCommitMessage(), files, new GitHub(getInput('GITHUB_TOKEN', {required: true})), _context);
 	} catch (error) {
 		setFailed(error.message);
 	}
