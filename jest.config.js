f8da5909caad2527a54d8882060bd7b384b6fde6
Technote <technote.space@gmail.com>
2020-03-05 17:19:42 +0900

    chore: tweaks

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
 	clearMocks: true,
-	moduleFileExtensions: [ 'js', 'ts' ],
-	setupFiles: [ '<rootDir>/jest.setup.ts' ],
+	moduleFileExtensions: ['js', 'ts'],
+	setupFiles: ['<rootDir>/jest.setup.ts'],
 	testEnvironment: 'node',
-	testMatch: [ '**/*.test.ts' ],
+	testMatch: ['**/*.test.ts'],
 	testRunner: 'jest-circus/runner',
 	transform: {
 		'^.+\\.ts$': 'ts-jest',
